d60b57bac8e37648a2545c05cb6ba76a82d21191
Admin <kbond@ngs.ru>
2015-11-16 23:38:07 +0600

    Task 2

---
+++ src/AcademTest2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Администратор on 16.11.15.
+ * 2.	Посчитайте среднее арифметическое четных чисел от 10 до 100
+ */
+public class AcademTest2 {
+    public static void main(String[] args) {
+
+        int j=0;
+        int sum=0;
+
+        for (int i=10; i<=100; i+=2){
+            sum+=i;
+            j++;
+        }
+        System.out.println(sum/j);
+    }
+}
