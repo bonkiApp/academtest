33fc2fd2b8202b7eb7660b07a0702ec49c2c2a9f
Admin <kbond@ngs.ru>
2015-11-22 22:51:52 +0600

    Something change

--- src/AcademTest3.java
+++ src/AcademTest3.java
@@ -21,19 +21,21 @@ public class AcademTest3 {
             if (a == 0 && b == 0) {
                 System.out.println("Уравнение не является квадратным и корней не имеет");
             } else if (a != 0) {
-                D = b * b - 4 * a * c;
+                D = b * b - 4 * a * c; //Формула дискириминанта
 
                 if (D == 0) {
                     x1 = x2 = (-b) / (2 * a);
+                    System.out.println("Уравнение имеет два одинаковых корня:");
                     System.out.println("x1 = " + x1);
                     System.out.println("x2 = " + x2);
                 } else if (D > 0) {
                     x1 = (-b + Math.pow(D, 1 / 2)) / (2 * a);
                     x2 = (-b - Math.pow(D, 1 / 2)) / (2 * a);
+                    System.out.println("Уравнение имеет два корня:");
                     System.out.println("x1 = " + x1);
                     System.out.println("x2 = " + x2);
                 } else {
-                    System.out.println("Уравнение имеет два мнимых корня");
+                    System.out.println("Уравнение имеет два мнимых корня:");
                     System.out.println("x1= (" + -b + "+i*" + Math.pow(D, 1 / 2) + ")" + "/" + (2 * a));
                     System.out.println("x2= (" + -b + "-i*" + Math.pow(D, 1 / 2) + ")" + "/" + (2 * a));
                 }
